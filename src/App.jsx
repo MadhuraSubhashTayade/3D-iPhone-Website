bbd7e002da3521e49b5e52ea11fcb02c329d8f83
Madhura Tayade <tayademadhura02@gmail.com>
2025-01-02 00:19:51 +0530

    iphone website till features section

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import Features from "./components/Features";
+import Hero from "./components/Hero";
+import Highlights from "./components/Highlights";
+import HowItWorks from "./components/HowItWorks";
+import Model from "./components/Model";
+import Navbar from "./components/Navbar";
+
+export default function App() {
+  return (
+    <main className="bg-black">
+      <Navbar />
+      <Hero />
+      <Highlights />
+      <Model />
+      <Features />
+      <HowItWorks />
+    </main>
+  );
+}
